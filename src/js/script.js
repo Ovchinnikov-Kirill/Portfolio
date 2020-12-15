8deaa326fbc6a0e00c3e877dcc883a05baf400e7
OvchinnikovKirill <Ovchinnikovk758@gmail.com>
2020-12-15 23:23:11 +0200

    promo complette

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeEL = document.querySelector('.menu__close');
+    menuOverlay = document.querySelector('.menu__overlay');
+
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeEL.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+menuOverlay.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
